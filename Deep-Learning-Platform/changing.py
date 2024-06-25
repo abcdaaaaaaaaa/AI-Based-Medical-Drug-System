82efda5f7b736f3230edf22406d943cc920660eb
abcdaaaaaaaaa <108553778+abcdaaaaaaaaa@users.noreply.github.com>
2024-06-25 09:19:41 +0300

    Delete Deep-Learning-Platform directory

--- Deep-Learning-Platform/changing.py
+++
@@ -1,2 +0,0 @@
-    parser.add_argument('--no-trace', action='store_false', help='don`t trace model')
-            for opt.weights in ['yolov7-e6e.pt']:
--- Deep-Learning-Platform/yolov7.sh
+++
@@ -1,4 +0,0 @@
-cd yolov7
-python -m venv myenv
-myenv\Scripts\activate #For Windows
-source myenv/bin/activate #For Linux
