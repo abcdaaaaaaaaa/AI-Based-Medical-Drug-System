d484d2634eb70cea4bdff2dc4bf3c1aaf1c91738
abcdaaaaaaaaa <108553778+abcdaaaaaaaaa@users.noreply.github.com>
2024-07-27 23:03:44 +0300

    Update index.php

--- doctor.uzay.info/index.php
+++ doctor.uzay.info/index.php
@@ -13,15 +13,18 @@ if (!isset($_SESSION['user_id'])) {
     <title>Sanal Doktor</title>
     <link rel="shortcut icon" href="bluespace.png">
     <link rel="stylesheet" href="styles.css">
+
 </head>
 <body>
 <div class="header">
     <?php if (isset($_SESSION['user_id'])): ?>
-        <a href="/logout" class="logout">Çıkış</a>
+        <a href="/logout" class="button logout">Çıkış</a>
+        <a href="/index.php" class="button restart">Yeniden Başlat</a>
     <?php else: ?>
-        <a href="/login" class="logout">Giriş</a>
+        <a href="/login" class="button logout">Giriş</a>
     <?php endif; ?>
 </div>
+
 <div class="container">
     <div id="question" class="question"></div>
     <div class="doctorImage">
@@ -33,14 +36,22 @@ if (!isset($_SESSION['user_id'])) {
         <div class="option" onclick="selectOption(3, 'Yuksek Ates ve Yorgunluk')">Yüksek Ateş ve Yorgunluk</div>
         <div class="option" onclick="selectOption(4, 'Goz ve Burun Alerjileri, Oksuruk, Burun Tikanikligi')">Göz ve Burun Alerjileri, Öksürük, Burun Tıkanıklıgı</div>
     </div>
-    <p id="selectedOptionText">Secilen Rahatsizlik Belirtisi:</p>
+    <p id="selectedOptionText">Seçilen Rahatsızlık Belirtisi:</p>
     <div id="doseForm" class="dose-form hidden">
-        <label for="age">Yaş (3'ten küçükse rasyonel sayı girin):</label>
-        <input type="number" id="age" name="age">
-        <label for="weight">Kilo (10'dan küçükse rasyonel sayı girin):</label>
-        <input type="number" id="weight" name="weight">
-        <button onclick="calculateDose()">Doz Hesapla</button>
+        <label for="age">Yaş:</label>
+        <input type="number" id="age" name="age" placeholder="Bitirdiğiniz yaşı buraya yazabilirsiniz.">
+        <label for="mounth">Ay:</label>
+        <input type="number" id="mounth" name="mounth" placeholder="0-11 arasında bşr ay tanımlayınız.">
+        <label for="weight">Kilo:</label>
+        <input type="number" id="weight" name="weight" placeholder="kg < 10 ise 5.3 gibi kesirli sayı giriniz.">
+        <button onclick="calculateDose()">Dozu Hesapla</button>
+    </div>
         <p id="doseResult" class="dose-result"></p>
+    <div id="inputContainerWrapper">
+        <div id="inputContainer">
+            <input type="fire" id="fireInput" placeholder="Örnek: Ateşim 37.2°C" />
+            <button id="submitButton">Gönder</button>
+        </div>
     </div>
 </div>
 <script>
